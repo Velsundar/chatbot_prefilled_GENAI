8cd465817652d8237ed4f08e3f14e64e3e205979
Sundaravel <sundaravel@neokred.tech>
2024-07-28 11:29:51 +0530

    similar words added

--- backend/chatbot.js
+++ backend/chatbot.js
@@ -1,18 +1,22 @@
 const Question = require('./models/Questions');
 const configSchema = require('./models/Config');
+const stringSimilarity = require('string-similarity');
 
 
 async function getBotResponse(message) {
-  const question = await Question.findOne({ question: message?.toLowerCase() });
+  const allQuestions = await Question.find({});
   const config = await configSchema.findOne();
-
   const chatbotName = config ? config.chatbotName : 'EVA';
-  
-  if (question) {
-    return question.answer;
-  } else {
-    return `I'm sorry, I don't understand that question. My name is ${chatbotName}, how can I assist you?`;
+
+  if (allQuestions.length > 0) {
+    const questionTexts = allQuestions.map(q => q.question);
+    const { bestMatch, bestMatchIndex } = stringSimilarity.findBestMatch(message?.toLowerCase(), questionTexts);
+    if (bestMatch.rating > 0.5) {
+      return allQuestions[bestMatchIndex].answer;
+    }
   }
+  
+  return `I'm sorry, I don't understand that question. My name is ${chatbotName}, how can I assist you?`;
 }
 
 module.exports = getBotResponse;
--- backend/controllers/chatbotControllers.js
+++ backend/controllers/chatbotControllers.js
@@ -2,6 +2,13 @@ const getBotResponse = require('../chatbot');
 
 exports.handleUserMessage = async (req, res) => {
   const userMessage = req.body.message;
-  const botResponse = await getBotResponse(userMessage);
-  res.json({ response: botResponse });
+  if (!userMessage) {
+    return res.status(400).json({ error: 'Request body must contain a message.' });
+  }
+  try {
+    const botResponse = await getBotResponse(userMessage);
+    res.json({ response: botResponse });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal server error.' });
+  }
 };
